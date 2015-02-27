33409856e5177addd82f9779499db5caf14cbdd1
dew-x <afigueras@enti.cat>
2015-02-27 10:56:14 +0100

    Added particle

--- Practica0/Practica0/main.cpp
+++ Practica0/Practica0/main.cpp
@@ -1,67 +1,31 @@
 #include <iostream>
-#include <iomanip>
-#include <glm\glm.hpp>
-#include <vector>
-#include "Point.h"
-#include "functions.h"
-#include "Sphere.h"
-#include "Plane.h"
-#include "Triangle.h"
-using namespace std;
+#include "Particle.h"
 
 
-void main() {
-	cout << setprecision(8);
-	float alpha = deg2rad(30.0);
-	float beta = deg2rad(45.0);
-	glm::vec3 direction = getVecFrom2Angles(alpha, beta);
-	vector<Point> points;
-	for (float i = 0; i <= 10; i += 0.01) {
-		Point p;
-		p.setCoord(direction.x*i, direction.y*i, direction.z*i);
-		points.push_back(p);
-	}
-	// sphere
-	Point center(1,2,1);
-	float radius = 3.0;
-	Sphere s(center, radius);
-	unsigned pos = 0;
-	bool dir = s.isPointInside(points[pos]);
-	while (pos < points.size() && dir==s.isPointInside(points[pos])) ++pos;
-	Line ls3(points[pos - 1], points[pos]);
-	bool valid;
-	Point inter1 = s.entryPointSegmentPlane(ls3, valid);
-	cout << "Shpere collision" << endl;
-	cout << "Collision between " << points[pos - 1] << " and " << points[pos] << endl;
-	cout << "Collision point: " << inter1 << endl << endl;
-	// plane
-	Point Q(1, 2, 1);
-	glm::vec3 norm = { -1, 1, 1 };
-	Plane p(Q, norm);
-	pos = 0;
-	dir = p.isInFront(points[0]);
-	while (pos < points.size() && dir==p.isInFront(points[pos])) ++pos;
-	Line ls1(points[pos - 1], points[pos]);
-	Point inter=p.entryPointSegmentPlane(ls1);
-	cout << "Plane collision" << endl;
-	cout << "Collision between " << points[pos - 1] << " and " << points[pos] <<  endl;
-	cout << "Collision point: " << inter << endl << endl;
-	// triangle
-	Point v1(1, 2, 1), v2(-1, 1, 2), v3(0, -1, 0);
-	Triangle t(v1, v2, v3);
-	pos = 0;
-	dir = t.tPlane->isInFront(points[0]);
-	while (pos < points.size() && dir == t.tPlane->isInFront(points[pos])) ++pos;
-	Line ls2(points[pos - 1], points[pos]);
-	bool valid1;
-	Point inter2 = t.entryPointSegmentPlane(ls2,valid1);
-	cout << "Triangle collision" << endl;
-	cout << "Collision between " << points[pos - 1] << " and " << points[pos] << endl;
-	if (valid1) {
-		cout << "Collision point: " << inter << endl << endl;
-	}
-	else {
-		cout << "Collision outside triangle." << endl << endl;
+void main(){
+	Particle p(0.0f, 10.0f, 0.0f);
+	float dt = 0.01f;
+	float tini = 0.0f;
+	float tfinal = 10.0f;
+	p.setLifetime(40.0f);
+	std::cout << "Lifetime =" << p.getLifetime() << std::endl;
+	p.setBouncing(0.8f);
+	p.addForce(0, -9.8f, 0);
+	p.setVelocity(0, 1, 0);
+	//	p.setFixed(true);
+
+	int count = 0;
+	for (float t = tini; t <= tfinal; t += dt){
+		p.updateParticle(dt, Particle::UpdateMethod::EulerSemi);
+		std::cout << "posicio = " << p.getCurrentPosition().x << "  " << p.getCurrentPosition().y
+			<< "  " << p.getCurrentPosition().z << "  temps = " << t << std::endl;
+		//Check Floor collisions
+		if (p.getCurrentPosition().y < 0.0f) { //only valid for the plane y=0 (floor plane)
+			p.setPosition(p.getCurrentPosition().x, -p.getCurrentPosition().y, p.getCurrentPosition().z);
+			p.setVelocity(p.getVelocity().x, -p.getVelocity().y, p.getVelocity().z);
+			std::cout << "rebot = " << count++ << std::endl;
+			system("PAUSE");
+		}
 	}
 	system("PAUSE");
 }
